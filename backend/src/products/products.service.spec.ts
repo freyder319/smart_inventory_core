6973ce47f0b734f23b7f9e508f6fdb959f9723a9
freyder319 <freyderjapo@gmail.com>
2025-12-28 08:40:42 -0500

    test: fix product service unit test with repository mock

--- backend/src/products/products.service.spec.ts
+++ backend/src/products/products.service.spec.ts
@@ -1,13 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let repo: Repository<Product>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ProductsService,
+        {
+          provide: getRepositoryToken(Product),
+          useValue: {
+            create: jest.fn().mockImplementation(dto => dto),
+            save: jest.fn().mockImplementation(product =>
+              Promise.resolve({ id: 1, ...product }),
+            ),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<ProductsService>(ProductsService);
+    repo = module.get<Repository<Product>>(getRepositoryToken(Product));
   });
 
-  it('should create a product', () => {
-    const product = service.create({
+  it('should create a product', async () => {
+    const product = await service.create({
       name: 'Mouse',
       price: 20000,
       currentStock: 10,
@@ -18,5 +40,6 @@ describe('ProductsService', () => {
     expect(product.id).toBe(1);
     expect(product.name).toBe('Mouse');
     expect(product.currentStock).toBe(10);
+    expect(product.minStock).toBe(5);
   });
 });
